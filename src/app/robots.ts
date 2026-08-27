63481172110d2d5f351686100a1e23e8fb1a51ea
chumkeara69-hue <chumkeara69@gmail.com>
2026-08-27 14:48:08 +0700

    Update robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -3,6 +3,6 @@ import type { MetadataRoute } from "next";
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: { userAgent: "*", allow: "/", disallow: "/admin/" },
-    sitemap: "https://tong-an-photography-2-im4z.vercel.app/sitemap.xml",
+  sitemap: "https://tong-an-photography-3-nntn.vercel.app/sitemap.xml",
   };
 }
